2f86d9029a9535ace6d392b491042a65341afce6
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-06-14 21:28:36 -0500

    bcrypt

--- server/dist/routes/usuarios.js
+++ server/dist/routes/usuarios.js
@@ -1,13 +1,19 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const user_models_1 = require("../models/user.models");
+const bcrypt_1 = __importDefault(require("bcrypt"));
 const usarRouter = (0, express_1.Router)();
 usarRouter.post('/create', (_req, _res) => {
+    const salt = bcrypt_1.default.genSaltSync(10);
+    const hash = bcrypt_1.default.hashSync(_req.body.password, salt);
     const user = {
         nombre: _req.body.nombre,
         email: _req.body.email,
-        password: _req.body.password,
+        password: hash,
         avatar: _req.body.avatar
     };
     user_models_1.Usuario.create(user).then(userDb => {
--- server/routes/usuarios.ts
+++ server/routes/usuarios.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from "express";
 import { Usuario } from "../models/user.models";
+import bcrypt from 'bcrypt' ;
 
 
 
@@ -7,11 +8,13 @@ const usarRouter = Router();
 
 usarRouter.post( '/create', ( _req: Request, _res: Response ) => {
 
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync( _req.body.password,salt);
 
     const user = {
         nombre: _req.body.nombre,
         email: _req.body.email,
-        password: _req.body.password,
+        password: hash,
         avatar: _req.body.avatar  
     }
 
