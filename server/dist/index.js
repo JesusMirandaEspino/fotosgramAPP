183ac28bb0c9732857b405d9667738cf44a65d47
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-06-14 21:04:11 -0500

    models

--- server/dist/index.js
+++ server/dist/index.js
@@ -5,8 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./classes/server"));
 const usuarios_1 = __importDefault(require("./routes/usuarios"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const server = new server_1.default();
 server.app.use('/user', usuarios_1.default);
+// conectar mongo
+mongoose_1.default.connect('mongodb://localhost:27017/fotosgram', (err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Base de datos Online');
+});
 // levantar express
 server.start(() => {
     console.log(`Servidor corriendo en puerto ${server.port}`);
--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,19 @@
 import Server from "./classes/server";
 import usarRouter from "./routes/usuarios";
+import mongoose from "mongoose";
 
 const server = new Server();
 
 
 server.app.use( '/user', usarRouter );
+
+
+// conectar mongo
+mongoose.connect( 'mongodb://localhost:27017/fotosgram', ( err ) => {
+    if( err ){ throw err; }
+    console.log( 'Base de datos Online' );
+});
+
 
 
 
