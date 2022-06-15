34750a317c46abdea720c78499a10b851a03c722
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-06-14 20:30:15 -0500

    init server

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,15 @@
-console.log('hola mundo');
+import Server from "./classes/server";
+
+const server = new Server();
+
+
+
+// levantar express
+
+
+server.start( () => {
+    console.log( `Servidor corriendo en puerto ${server.port}` );
+});
+
+
+
